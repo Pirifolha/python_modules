5fb8972c1b1a92ecb31094f5a4f1485ae3d48cc1
Miguel Sousa <misousa@c1r6s9.42lisboa.com>
2026-04-22 22:13:32 +0100

    finished py06

--- py06/alchemy/transmutation/recipes.py
+++ py06/alchemy/transmutation/recipes.py
@@ -3,8 +3,8 @@
 import alchemy
 from alchemy.potions import create_fire
 
+
 def lead_to_gold() -> str:
-    
 
     return (
         f"Recipe transmuting Lead to Gold: brew '{alchemy.create_air()}'"
