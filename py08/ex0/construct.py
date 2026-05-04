47c6e10c4e62c9aa683cfb8a0a9262660251e390
Miguel Sousa <miguelsousa218@gmail.com>
2026-05-04 21:33:04 +0100

    added 8, ex0

---
+++ py08/ex0/construct.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import site
+import sys
+import os
+
+
+def neg_venv() -> None:
+    print("\nMATRIX STATUS: You're still plugged in\n")
+    print(
+        f"Current Python: {sys.executable}\n"
+        f"Virtual Enviroment: None detected"
+        "WARNING: You're in the global environment!\n"
+        "The machines can see everything you install.\n"
+        "To enter the cosntruct, run:\n"
+        "python -m venv matrix_env\n"
+        "source matrix_env/bin/activate # On Unix\n"
+        r"matrix_env\Scripts\activate # On Windows\n"
+    )
+    print("Then run this program again")
+
+
+def pos_venv() -> None:
+    print("\nMATRIX STATUS: Welcome to the construct\n")
+    print(
+        f"Current Python: {sys.executable}\n"
+        f"Virtual Enviroment: {os.path.basename(sys.prefix)}\n"
+        f"Environment Path: {sys.prefix}\n"
+    )
+    print(
+        "SUCCESS: You're in an isolated environment!\n"
+        "Safe to install packages without affecting\n"
+        "the global system.\n"
+    )
+    print("Package installation path:\n" f"{site.getsitepackages()[0]}")
+
+
+def test() -> None:
+    if (
+        hasattr(sys, "real_prefix")
+        or (hasattr(sys, "base_prefix"))
+        and sys.prefix != sys.base_prefix
+    ):
+        pos_venv()
+    else:
+        neg_venv()
+
+
+if __name__ == "__main__":
+    test()
